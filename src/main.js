e5b0037aea3d7d19f22853b6a14a1977db973945
Huy Khanh <khanhthce@gmail.com>
2022-03-19 14:20:12 +0700

    update nodemon

--- src/main.js
+++ src/main.js
@@ -77,5 +77,5 @@ io.on('connection', (socket) => {
     })
 
 });
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 server.listen(port,console.log(`app is runing with port ${port}`));
